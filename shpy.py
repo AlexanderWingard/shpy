d6f0a6e458b5d7908ee9f5869e453c40770af152
Alexander Wingård <alexander.wingard@gmail.com>
2015-05-26 22:50:31 +0200

    Rewrite using threads

--- shpy.py
+++ shpy.py
@@ -1,55 +1,44 @@
 import logging
 import subprocess
-import sys
-import select
 import shlex
-from fcntl import fcntl, F_GETFL, F_SETFL
-from os import O_NONBLOCK
-from cStringIO import StringIO
+from subprocess import Popen, PIPE
+from threading import Thread
+from Queue import Queue, Empty
 
-logging.basicConfig(format="%(asctime)s %(out)3s %(prog)8s %(message)s", level=logging.DEBUG)
+#logging.basicConfig(format="%(asctime)s %(out)6s %(prog)8s | %(message)s", level=logging.DEBUG)
+logging.basicConfig(format="%(asctime)s %(out)6s | %(message)s", level=logging.DEBUG)
+
+def c(str, *args, **kwargs):
+    jobs = Queue()
+    def line_reader(pipe, result, label):
+        jobs.get()
+        for line in iter(pipe.readline, b''):
+            line = line.rstrip()
+            logging.info(line, extra={'prog':str, 'out':label})
+            result.append(line)
+        jobs.task_done()
+
+    def pipe_watch(pipe, label):
+        jobs.put(1)
+        result = []
+        t = Thread(target=line_reader, args=[pipe, result, label])
+        t.daemon = True
+        t.start()
+        return result
 
 
-def c(str, *args):
-    p = subprocess.Popen(shlex.split(str.format(*args)),
+    cl = str.format(*args)
+    logging.info(cl, extra={'prog':'', 'out':'CALL'})
+    p = subprocess.Popen(shlex.split(cl),
                          stdout = subprocess.PIPE,
                          stderr = subprocess.PIPE,
                          close_fds=True)
 
-    flags = fcntl(p.stdout, F_GETFL)
-    fcntl(p.stdout, F_SETFL, flags | O_NONBLOCK)
-    flags = fcntl(p.stderr, F_GETFL)
-    fcntl(p.stderr, F_SETFL, flags | O_NONBLOCK)
-
-
-    stdout = StringIO()
-    stderr = StringIO()
-
-    while True:
-        reads = [p.stdout.fileno(), p.stderr.fileno()]
-        ret = select.select(reads, [], [])
-
-        for fd in ret[0]:
-            if fd == p.stdout.fileno():
-                try:
-                    for read in iter(p.stdout.readline, b''):
-                        stdout.write(read)
-                        logging.info(read.rstrip(), extra={'prog':str, 'out':'out'})
-                except:
-                    pass
-
-            if fd == p.stderr.fileno():
-                try:
-                    for read in iter(p.stderr.readline, b''):
-                        stderr.write(read)
-                        logging.info(read.rstrip(), extra={'prog':str, 'out':'err'})
-                except:
-                    pass
-
-        if p.poll() != None:
-            break
-
-    print 'program ended ', p.returncode
-    print 'stdout:\n', stdout.getvalue()
-    print 'stderr:\n', stderr.getvalue()
-
+    outres = pipe_watch(p.stdout, "OUT")
+    errres = pipe_watch(p.stderr, "ERROR")
+    rescode = p.wait()
+    jobs.join()
+    logging.info(rescode, extra={'prog':'', 'out':'RETURN'})
+    #if not rescode == 0:
+    #    raise subprocess.CalledProcessError(rescode, cmd=cl, output="\n".join(errres))
+    return outres
